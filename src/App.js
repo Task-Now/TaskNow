96da9080ef8f0cfbf4f1dfcabff8cec6d9676de5
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-03-20 12:43:25 -0300

    Added Routes and AuthProvider

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { AuthProvider } from "./contexts/AuthContext";
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import HomePage from "./pages/HomePage";
+
+function App() {
+  return (
+    <Router>
+      <AuthProvider>
+        <Switch>
+          <Route exact path="/" component={HomePage}/>
+        </Switch>
+      </AuthProvider>
+    </Router>
+  );
+}
+
+export default App;
