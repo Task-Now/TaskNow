d2d4ed532ce93ad6e53036e8febbb893126e2d40
victorgrodriguesm7 <victorgrodriguesm7@gmail.com>
2021-03-22 17:20:06 -0300

    HomePage content added

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -4,8 +4,25 @@ import './index.css';
 
 export default function HomePage() {
     return (
-        <div>
+        <div className="home-page">
             <Header/>
+            <main className="content">
+                <div className="left">
+                    <p className="title">
+                        Administrar trabalhos escolares nunca foi <strong>tão fácil</strong>
+                    </p>
+                    <p className="slogan">
+                        A ferramenta número 1 voltada para administração de trabalhos escolares e acadêmicos
+                    </p>
+                    <button type="button" className="get-start">
+                        Vamos começar &#8594;
+                    </button>
+                </div>
+                <div className="login-box">
+
+                </div>
+            </main>
+            <img className="watermelon" src="assets/watermelon.svg" alt="Watermelon"/>
         </div>
     );
 }
